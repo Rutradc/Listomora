cb588fc991313fe39d5fd6d4e02fb8832c63e976
Rutradc <arthurdc@live.be>
2026-03-09 09:56:00 +0100

    fixed errorMessage from login maybe ?

--- ListomoraFront/Pages/Auth/Login.razor.cs
+++ ListomoraFront/Pages/Auth/Login.razor.cs
@@ -19,7 +19,8 @@ namespace ListomoraFront.Pages.Auth
         private bool isShown = false;
         private InputType PasswordInput = InputType.Password;
         private string PasswordInputIcon = Icons.Material.Filled.VisibilityOff;
-        //TODO: fix errorMessage qui se montre avec "Email ou mot de passe incorrect." avant de charger et de connecter l'utilisateur
+        // bug todo n'arrive plus sans rien avoir changé --> gardé pour postérité au cas où
+        //----------------------------  TODO: fix errorMessage qui se montre avec "Email ou mot de passe incorrect." avant de charger et de connecter l'utilisateur
         private async Task HandleLogin()
         {
             isLoggingIn = true;
