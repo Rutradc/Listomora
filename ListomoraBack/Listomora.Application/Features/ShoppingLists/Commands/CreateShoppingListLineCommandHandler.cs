b4abbf9255c07a15a4cd852ca5934f0c3b458e01
Rutradc <arthurdc@live.be>
2026-03-13 14:16:42 +0100

    quick fix for commandHandler

--- ListomoraBack/Listomora.Application/Features/ShoppingLists/Commands/CreateShoppingListLineCommandHandler.cs
+++ ListomoraBack/Listomora.Application/Features/ShoppingLists/Commands/CreateShoppingListLineCommandHandler.cs
@@ -6,6 +6,12 @@ namespace Listomora.Application.Features.ShoppingLists.Commands
     public class CreateShoppingListLineCommandHandler : IRequestHandler<CreateShoppingListLineCommand, Unit>
     {
         private readonly IShoppingListRepo _repo;
+
+        public CreateShoppingListLineCommandHandler(IShoppingListRepo repo)
+        {
+            _repo = repo;
+        }
+
         public async Task<Unit> Handle(CreateShoppingListLineCommand request, CancellationToken cancellationToken)
         {
             await _repo.InsertLineAsync(request.Dto);
