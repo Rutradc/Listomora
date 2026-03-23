99a7d136f2ed6b8efba7afefef7b650aac19c7ef
Rutradc <arthurdc@live.be>
2026-03-23 11:14:24 +0100

    Refactor to seeds by using SeedIds static class

--- ListomoraBack/Listomora.Infrastructure/Seeds/IngredientSeed.cs
+++ ListomoraBack/Listomora.Infrastructure/Seeds/IngredientSeed.cs
@@ -2,6 +2,7 @@
 using Listomora.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace Listomora.Infrastructure.Seeds
 {
     public class IngredientSeed : IEntityTypeConfiguration<Ingredient>
@@ -10,258 +11,42 @@ namespace Listomora.Infrastructure.Seeds
         {
             #region BaseSeed
             builder.HasData(
-                new
-                {
-                    Id = new Guid("cae52516-7547-4d4e-a289-358fd8891a98"),
-                    Name = "Pommes de terre",
-                    IsPublic = true,
-                    Category = IngredientCategory.CEREALS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("e5bebf1e-bd35-448c-a1a2-941e55687ff5"),
-                    Name = "Viande de veau",
-                    IsPublic = false,
-                    Category = IngredientCategory.MEAT,
-                    CreatorId = new Guid("6ad52029-0225-48c4-a2b5-7aa35fec7056"),
-                },
-                new
-                {
-                    Id = new Guid("1ed84abe-4c15-458f-82b8-a5a9bc34b5b5"),
-                    Name = "Filet de saumon",
-                    IsPublic = true,
-                    Category = IngredientCategory.FISH_AND_SEAFOODS,
-                    CreatorId = new Guid("0eb2993d-7fd5-4f29-9172-1b8f6aa80736"),
-                },
-                new
-                {
-                    Id = new Guid("82a56879-1d59-40f6-8a20-75cdcd2fe686"),
-                    Name = "Emmental rapé",
-                    IsPublic = true,
-                    Category = IngredientCategory.DAIRY_PRODUCTS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("97505262-64f9-4444-83cf-67c93d8161d5"),
-                    Name = "Blanc de poulet",
-                    IsPublic = false,
-                    Category = IngredientCategory.MEAT,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("97796814-4f88-40a6-8d3d-98f46135be41"),
-                    Name = "Sel",
-                    IsPublic = true,
-                    Category = IngredientCategory.CONDIMENTS,
-                    CreatorId = new Guid("6ad52029-0225-48c4-a2b5-7aa35fec7056"),
-                },
-                new
-                {
-                    Id = new Guid("16c4a654-0f04-40eb-a23e-9d580c7062a8"),
-                    Name = "Poivre",
-                    IsPublic = false,
-                    Category = IngredientCategory.CONDIMENTS,
-                    CreatorId = new Guid("0eb2993d-7fd5-4f29-9172-1b8f6aa80736"),
-                },
-                new
-                {
-                    Id = new Guid("3d11a56b-1b6a-4581-940a-36eb4c25d01c"),
-                    Name = "Spaghetti",
-                    IsPublic = true,
-                    Category = IngredientCategory.CEREALS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                }
+                new { Id = SeedIds.Ingredients.PommesDeTerre, Name = "Pommes de terre", IsPublic = true, Category = IngredientCategory.CEREALS, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.ViandeVeau, Name = "Viande de veau", IsPublic = false, Category = IngredientCategory.MEAT, CreatorId = SeedIds.Users.CristianoRonaldo },
+                new { Id = SeedIds.Ingredients.Saumon, Name = "Filet de saumon", IsPublic = true, Category = IngredientCategory.FISH_AND_SEAFOODS, CreatorId = SeedIds.Users.LionelMessi },
+                new { Id = SeedIds.Ingredients.Emmental, Name = "Emmental rapé", IsPublic = true, Category = IngredientCategory.DAIRY_PRODUCTS, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.BlancPoulet, Name = "Blanc de poulet", IsPublic = false, Category = IngredientCategory.MEAT, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Sel, Name = "Sel", IsPublic = true, Category = IngredientCategory.CONDIMENTS, CreatorId = SeedIds.Users.CristianoRonaldo },
+                new { Id = SeedIds.Ingredients.Poivre, Name = "Poivre", IsPublic = false, Category = IngredientCategory.CONDIMENTS, CreatorId = SeedIds.Users.LionelMessi },
+                new { Id = SeedIds.Ingredients.Spaghetti, Name = "Spaghetti", IsPublic = true, Category = IngredientCategory.CEREALS, CreatorId = SeedIds.Users.JohnCena }
             );
             #endregion
+
             #region SeedForShoppingList
             builder.HasData(
-                new
-                {
-                    Id = new Guid("ac07af80-640a-4270-9d48-62f8c9b84df6"),
-                    Name = "Haricots verts",
-                    IsPublic = true,
-                    Category = IngredientCategory.LEGUMES,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("2145139b-64b4-475e-ba84-5efe83d53d8b"),
-                    Name = "Cannelle",
-                    IsPublic = true,
-                    Category = IngredientCategory.CONDIMENTS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("4c500d54-1468-4a8c-81f5-ff846fb36b3d"),
-                    Name = "Moutarde",
-                    IsPublic = true,
-                    Category = IngredientCategory.CONDIMENTS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("4a5bda61-ac90-4faf-9ae1-139f81a17eb6"),
-                    Name = "Fraise",
-                    IsPublic = true,
-                    Category = IngredientCategory.FRUITS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("b09ea0c6-b53a-4a70-bff7-56dcfc5bb2d0"),
-                    Name = "Pomme",
-                    IsPublic = true,
-                    Category = IngredientCategory.FRUITS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("e6f1fd6a-abb7-4d84-bca8-aeff139dc88c"),
-                    Name = "Tomate",
-                    IsPublic = true,
-                    Category = IngredientCategory.VEGETABLES,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("eaa7d8cb-f6ac-40b6-9cfe-6250acb41907"),
-                    Name = "Brocoli",
-                    IsPublic = true,
-                    Category = IngredientCategory.VEGETABLES,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("46703052-e35c-4582-85a8-418f284e0ec8"),
-                    Name = "Mozzarella",
-                    IsPublic = true,
-                    Category = IngredientCategory.DAIRY_PRODUCTS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("9e736920-7440-4376-b485-1f192e0d7470"),
-                    Name = "Poulet",
-                    IsPublic = true,
-                    Category = IngredientCategory.MEAT,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("c762d216-d6ab-43b6-bfe7-cab995c5aea9"),
-                    Name = "Saucisses",
-                    IsPublic = true,
-                    Category = IngredientCategory.MEAT,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("794f67c2-7afb-495e-8570-6daad6cb4a0a"),
-                    Name = "Carotte",
-                    IsPublic = true,
-                    Category = IngredientCategory.VEGETABLES,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("fcf05aed-90e9-47a0-b9b9-09af9bf0f2b9"),
-                    Name = "Huile d'olive",
-                    IsPublic = true,
-                    Category = IngredientCategory.FATS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("48f07af2-eefb-4097-a479-cc50a3117851"),
-                    Name = "Beurre",
-                    IsPublic = true,
-                    Category = IngredientCategory.FATS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("121a810d-3904-4cf1-9af4-063a2e56b62b"),
-                    Name = "Thon",
-                    IsPublic = true,
-                    Category = IngredientCategory.FISH_AND_SEAFOODS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("edf88fdd-0117-4aa4-b3a5-17be26c913f9"),
-                    Name = "Lait",
-                    IsPublic = true,
-                    Category = IngredientCategory.BEVERAGES,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("1dd4a5f7-f93f-42ed-919d-c677f0dc0e7f"),
-                    Name = "Chocolat",
-                    IsPublic = true,
-                    Category = IngredientCategory.SWEET_PRODUCTS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("21d579a9-b2ae-4434-b7d5-45201001bca1"),
-                    Name = "Sucre",
-                    IsPublic = true,
-                    Category = IngredientCategory.SWEET_PRODUCTS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("c65e7d76-8af8-4e1c-b89e-3102248e73a6"),
-                    Name = "Crevettes",
-                    IsPublic = true,
-                    Category = IngredientCategory.FISH_AND_SEAFOODS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("aac50be3-c2ea-4642-a3d9-81f58299e012"),
-                    Name = "Pêche",
-                    IsPublic = true,
-                    Category = IngredientCategory.FRUITS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("d35633e3-34cb-48d2-aedc-5a65271357d2"),
-                    Name = "Pain",
-                    IsPublic = true,
-                    Category = IngredientCategory.BAKED_AND_PASTRIES,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("3d956419-cd5b-41c5-9b2a-d7e5a990c838"),
-                    Name = "Œufs",
-                    IsPublic = true,
-                    Category = IngredientCategory.EGGS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("924c8c5b-9684-4b77-b5d0-80b0f1cf8b30"),
-                    Name = "Persil",
-                    IsPublic = true,
-                    Category = IngredientCategory.HERBS_AND_SPICES,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                },
-                new
-                {
-                    Id = new Guid("ff0cdcf7-aee0-4b9e-b520-8d0fb3b93047"),
-                    Name = "Macaroni",
-                    IsPublic = true,
-                    Category = IngredientCategory.CEREALS,
-                    CreatorId = new Guid("5bd4bf86-da80-438c-be55-a466ea3b994d"),
-                }
+                new { Id = SeedIds.Ingredients.HaricotsVerts, Name = "Haricots verts", IsPublic = true, Category = IngredientCategory.LEGUMES, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Cannelle, Name = "Cannelle", IsPublic = true, Category = IngredientCategory.CONDIMENTS, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Moutarde, Name = "Moutarde", IsPublic = true, Category = IngredientCategory.CONDIMENTS, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Fraise, Name = "Fraise", IsPublic = true, Category = IngredientCategory.FRUITS, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Pomme, Name = "Pomme", IsPublic = true, Category = IngredientCategory.FRUITS, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Tomate, Name = "Tomate", IsPublic = true, Category = IngredientCategory.VEGETABLES, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Brocoli, Name = "Brocoli", IsPublic = true, Category = IngredientCategory.VEGETABLES, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Mozzarella, Name = "Mozzarella", IsPublic = true, Category = IngredientCategory.DAIRY_PRODUCTS, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Poulet, Name = "Poulet", IsPublic = true, Category = IngredientCategory.MEAT, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Saucisses, Name = "Saucisses", IsPublic = true, Category = IngredientCategory.MEAT, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Carotte, Name = "Carotte", IsPublic = true, Category = IngredientCategory.VEGETABLES, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.HuileOlive, Name = "Huile d'olive", IsPublic = true, Category = IngredientCategory.FATS, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Beurre, Name = "Beurre", IsPublic = true, Category = IngredientCategory.FATS, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Thon, Name = "Thon", IsPublic = true, Category = IngredientCategory.FISH_AND_SEAFOODS, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Lait, Name = "Lait", IsPublic = true, Category = IngredientCategory.BEVERAGES, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Chocolat, Name = "Chocolat", IsPublic = true, Category = IngredientCategory.SWEET_PRODUCTS, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Sucre, Name = "Sucre", IsPublic = true, Category = IngredientCategory.SWEET_PRODUCTS, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Crevettes, Name = "Crevettes", IsPublic = true, Category = IngredientCategory.FISH_AND_SEAFOODS, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Peche, Name = "Pêche", IsPublic = true, Category = IngredientCategory.FRUITS, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Pain, Name = "Pain", IsPublic = true, Category = IngredientCategory.BAKED_AND_PASTRIES, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Oeufs, Name = "Œufs", IsPublic = true, Category = IngredientCategory.EGGS, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Persil, Name = "Persil", IsPublic = true, Category = IngredientCategory.HERBS_AND_SPICES, CreatorId = SeedIds.Users.JohnCena },
+                new { Id = SeedIds.Ingredients.Macaroni, Name = "Macaroni", IsPublic = true, Category = IngredientCategory.CEREALS, CreatorId = SeedIds.Users.JohnCena }
             );
             #endregion
         }
---
+++ ListomoraBack/Listomora.Infrastructure/Seeds/SeedIds.cs
@@ -0,0 +1,119 @@
+﻿namespace Listomora.Infrastructure.Seeds
+{
+    public static class SeedIds
+    {
+        public static class Users
+        {
+            public static readonly Guid JohnCena = new("5bd4bf86-da80-438c-be55-a466ea3b994d");
+            public static readonly Guid CristianoRonaldo = new("6ad52029-0225-48c4-a2b5-7aa35fec7056");
+            public static readonly Guid LionelMessi = new("0eb2993d-7fd5-4f29-9172-1b8f6aa80736");
+        }
+
+        public static class Articles
+        {
+            public static readonly Guid RaquettePadel = new("bdf2a31b-5e8f-4874-838d-3246a804ae13");
+            public static readonly Guid PapierToilette = new("6a03f6d7-9160-4d0f-b94b-dbc41d497ec8");
+            public static readonly Guid Crayons = new("34a8bda4-f77f-42e5-ba61-2b511985c4db");
+            public static readonly Guid ProduitVaisselle = new("bb861dfa-889d-49c3-ad76-407d662dd7c2");
+            public static readonly Guid SacsPoubelle = new("06effa04-6bcf-401f-a091-00b24f6ba8f0");
+            public static readonly Guid Deodorant = new("f15deb6c-e587-421c-b0cf-1ea1640df7b3");
+
+            public static readonly Guid SavonPalmolive = new("d6acb86d-856e-4874-9d64-73a1f3e2f8e9");
+            public static readonly Guid Mouchoirs = new("6ef48e96-a40a-4dc5-9432-fe6d85c87bb6");
+            public static readonly Guid CocaCola = new("c7e03ae1-353c-4e21-963f-ff906d09506c");
+            public static readonly Guid Fanta = new("be258d12-1679-4aa2-8b71-368b2c16b913");
+            public static readonly Guid PizzaSurgelee = new("d392cdc1-9141-4dc3-ac7e-a4486e2e8774");
+            public static readonly Guid ChipsKroky = new("336ac8ab-c997-4521-a3c1-9aaa5cb86fbb");
+            public static readonly Guid Dentifrice = new("a456432d-33d1-44d9-be6a-8599bd02becc");
+            public static readonly Guid PilesAAA = new("81bd4a7b-b60c-4e11-8d74-6c641721a928");
+        }
+
+        public static class Ingredients
+        {
+            public static readonly Guid PommesDeTerre = new("cae52516-7547-4d4e-a289-358fd8891a98");
+            public static readonly Guid ViandeVeau = new("e5bebf1e-bd35-448c-a1a2-941e55687ff5");
+            public static readonly Guid Saumon = new("1ed84abe-4c15-458f-82b8-a5a9bc34b5b5");
+            public static readonly Guid Emmental = new("82a56879-1d59-40f6-8a20-75cdcd2fe686");
+            public static readonly Guid BlancPoulet = new("97505262-64f9-4444-83cf-67c93d8161d5");
+            public static readonly Guid Sel = new("97796814-4f88-40a6-8d3d-98f46135be41");
+            public static readonly Guid Poivre = new("16c4a654-0f04-40eb-a23e-9d580c7062a8");
+            public static readonly Guid Spaghetti = new("3d11a56b-1b6a-4581-940a-36eb4c25d01c");
+
+            public static readonly Guid HaricotsVerts = new("ac07af80-640a-4270-9d48-62f8c9b84df6");
+            public static readonly Guid Cannelle = new("2145139b-64b4-475e-ba84-5efe83d53d8b");
+            public static readonly Guid Moutarde = new("4c500d54-1468-4a8c-81f5-ff846fb36b3d");
+            public static readonly Guid Fraise = new("4a5bda61-ac90-4faf-9ae1-139f81a17eb6");
+            public static readonly Guid Pomme = new("b09ea0c6-b53a-4a70-bff7-56dcfc5bb2d0");
+            public static readonly Guid Tomate = new("e6f1fd6a-abb7-4d84-bca8-aeff139dc88c");
+            public static readonly Guid Brocoli = new("eaa7d8cb-f6ac-40b6-9cfe-6250acb41907");
+            public static readonly Guid Mozzarella = new("46703052-e35c-4582-85a8-418f284e0ec8");
+            public static readonly Guid Poulet = new("9e736920-7440-4376-b485-1f192e0d7470");
+            public static readonly Guid Saucisses = new("c762d216-d6ab-43b6-bfe7-cab995c5aea9");
+            public static readonly Guid Carotte = new("794f67c2-7afb-495e-8570-6daad6cb4a0a");
+            public static readonly Guid HuileOlive = new("fcf05aed-90e9-47a0-b9b9-09af9bf0f2b9");
+            public static readonly Guid Beurre = new("48f07af2-eefb-4097-a479-cc50a3117851");
+            public static readonly Guid Thon = new("121a810d-3904-4cf1-9af4-063a2e56b62b");
+            public static readonly Guid Lait = new("edf88fdd-0117-4aa4-b3a5-17be26c913f9");
+            public static readonly Guid Chocolat = new("1dd4a5f7-f93f-42ed-919d-c677f0dc0e7f");
+            public static readonly Guid Sucre = new("21d579a9-b2ae-4434-b7d5-45201001bca1");
+            public static readonly Guid Crevettes = new("c65e7d76-8af8-4e1c-b89e-3102248e73a6");
+            public static readonly Guid Peche = new("aac50be3-c2ea-4642-a3d9-81f58299e012");
+            public static readonly Guid Pain = new("d35633e3-34cb-48d2-aedc-5a65271357d2");
+            public static readonly Guid Oeufs = new("3d956419-cd5b-41c5-9b2a-d7e5a990c838");
+            public static readonly Guid Persil = new("924c8c5b-9684-4b77-b5d0-80b0f1cf8b30");
+            public static readonly Guid Macaroni = new("ff0cdcf7-aee0-4b9e-b520-8d0fb3b93047");
+        }
+
+        public static class ShoppingLists
+        {
+            public static readonly Guid ListeSamedi28Mars = new("86a0fcf5-f62e-4809-a28b-a9403514991e");
+            public static readonly Guid ListeMercredi = new("f132fcbf-bcd5-4ce9-ba7b-0dc102fabb72");
+            public static readonly Guid ListeSamedi7Mars = new("5e47b154-be10-43bb-ad15-a09f83d77ff0");
+            public static readonly Guid ListeHabituelle = new("49c7a8c0-0647-4380-a588-e8d88c039f06");
+        }
+
+        public static class ShoppingListLines
+        {
+            #region ListeSamedi28Mars
+            public static readonly Guid ListeSamedi28Mars_1 = Guid.Parse("0bcd6b9c-0ac1-4b21-b7ac-ec31808d3a8c");
+            public static readonly Guid ListeSamedi28Mars_2 = Guid.Parse("8c0199b7-0569-4ea8-adfc-455113d6a319");
+            public static readonly Guid ListeSamedi28Mars_3 = Guid.Parse("b9f22fa9-779c-45be-8608-60b429145d0a");
+            public static readonly Guid ListeSamedi28Mars_4 = Guid.Parse("6a475d95-febf-4e24-8f05-5e31452d0d49");
+            public static readonly Guid ListeSamedi28Mars_5 = Guid.Parse("9b42859a-1e6b-46cb-8dce-e745c8fdbc4f");
+            public static readonly Guid ListeSamedi28Mars_6 = Guid.Parse("204138ba-fb1b-4410-b345-8c1ad81ffa47");
+            public static readonly Guid ListeSamedi28Mars_7 = Guid.Parse("fee9786c-4d96-44f9-9b2b-5980e9db5802");
+            #endregion
+
+            #region ListeMercredi
+            public static readonly Guid ListeMercredi_1 = Guid.Parse("bbaaa9a3-591f-46e6-8598-1dcb397add1a");
+            public static readonly Guid ListeMercredi_2 = Guid.Parse("9f5d755d-eab1-4c40-8f12-2271ac8413ff");
+            public static readonly Guid ListeMercredi_3 = Guid.Parse("37b52e9a-6b26-4c4b-a4ed-df5c5105d64b");
+            public static readonly Guid ListeMercredi_4 = Guid.Parse("c1f6a7c1-0f40-468b-89c2-e76f50cc5781");
+            public static readonly Guid ListeMercredi_5 = Guid.Parse("dfb86232-fab9-4cff-8b6a-ec25d98171da");
+            public static readonly Guid ListeMercredi_6 = Guid.Parse("85589ef1-201f-44a1-85ad-3dd3d3b27326");
+            public static readonly Guid ListeMercredi_7 = Guid.Parse("a4930d78-4c79-4207-ad38-d329971ae88d");
+            public static readonly Guid ListeMercredi_8 = Guid.Parse("0f031734-21c3-4978-831d-bdc4f66527ac");
+            #endregion
+
+            #region ListeSamedi7Mars
+            public static readonly Guid ListeSamedi7Mars_1 = Guid.Parse("be147985-dfa5-417c-815c-822fb15ee073");
+            public static readonly Guid ListeSamedi7Mars_2 = Guid.Parse("738afb77-0f6e-44c6-9fb6-9ab850d94b94");
+            public static readonly Guid ListeSamedi7Mars_3 = Guid.Parse("800ca337-3bb0-4108-ab8b-895d392e09c6");
+            public static readonly Guid ListeSamedi7Mars_4 = Guid.Parse("1666bda5-7278-4b68-9416-27d0195c1a75");
+            public static readonly Guid ListeSamedi7Mars_5 = Guid.Parse("78313e81-5a69-4869-99ce-733fc6023dcb");
+            public static readonly Guid ListeSamedi7Mars_6 = Guid.Parse("eb2698fc-8e3e-49f3-b6ee-7f128a0eea45");
+            public static readonly Guid ListeSamedi7Mars_7 = Guid.Parse("12e92f83-af4f-4c86-aace-404e9466b69b");
+            #endregion
+
+            #region ListeHabituelle
+            public static readonly Guid ListeHabituelle_1 = Guid.Parse("e18dc687-be41-475a-be2f-38a31ccfa510");
+            public static readonly Guid ListeHabituelle_2 = Guid.Parse("58ae0ec0-78a1-41a6-bca8-63152e124dbb");
+            public static readonly Guid ListeHabituelle_3 = Guid.Parse("76b7acb0-61ac-42fa-9af3-214fb07b7342");
+            public static readonly Guid ListeHabituelle_4 = Guid.Parse("a2fe4f08-aff4-4044-a21d-eaccc7338d72");
+            public static readonly Guid ListeHabituelle_5 = Guid.Parse("ca76e795-7768-48b5-8d32-5fae47084450");
+            public static readonly Guid ListeHabituelle_6 = Guid.Parse("2144a4da-75da-4dde-b3d9-0ff144a67d31");
+            public static readonly Guid ListeHabituelle_7 = Guid.Parse("b7635cb2-3962-472b-94bc-9528f53af09e");
+            #endregion
+        }
+    }
+}
